5ab89b9bfec533b03234bc3c6a05d3711f9cabc0
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-01 17:15:10 +0100

    delete dummy block (not needed anymore), pylint

--- udapi/block/demo/rehangprepositions.py
+++ udapi/block/demo/rehangprepositions.py
@@ -1,8 +1,9 @@
+"""RehangPrepositions demo block."""
 from udapi.core.block import Block
 
 
 class RehangPrepositions(Block):
-
+    """This block takes all prepositions (upos=ADP) and rehangs them above their parent."""
     def process_node(self, node):
         if node.upos == "ADP":
             origparent = node.parent
