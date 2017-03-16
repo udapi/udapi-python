a364bd76736fff4975c4af46913cac3e4ce7576d
Martin Popel <popel@ufal.mff.cuni.cz>
2017-03-16 19:19:13 +0100

    new blocks ud.RemoveMwt and ud.cs.AddMwt
    Plus abstract base class ud.AddMwt.

---
+++ udapi/block/ud/addmwt.py
@@ -0,0 +1,69 @@
+"""Abstract base class ud.AddMwt for heuristic detection of multi-word tokens."""
+from udapi.core.block import Block
+
+class AddMwt(Block):
+    """Detect and mark MWTs (split them into words and add the words to the tree)."""
+
+    def process_node(self, node):
+        analysis = self.multiword_analysis(node)
+        if analysis is None:
+            return
+        orig_attr = {}
+        for attr in 'form lemma upos xpos feats deprel misc'.split():
+            orig_attr[attr] = getattr(node, attr)
+
+        forms = analysis['form'].split()
+        main = analysis.get('main', 0)
+        parent = node if analysis.get('shape', '') == 'subtree' else node.parent
+        nodes = []
+        for form in forms[0:main]:
+            new_node = parent.create_child(form=form)
+            new_node.shift_before_node(node)
+            nodes.append(new_node)
+        node.form = forms[main]
+        nodes.append(node)
+        for form in forms[main+1:]:
+            new_node = parent.create_child(form=form)
+            new_node.shift_after_node(nodes[-1])
+            nodes.append(new_node)
+
+        if orig_attr['form'].isupper():
+            for new_node in nodes:
+                new_node.form = new_node.form.upper()
+        elif orig_attr['form'][0].isupper():
+            nodes[0].form = nodes[0].form.title()
+
+        for attr in 'lemma upos xpos feats deprel misc'.split():
+            if attr in analysis:
+                values = analysis[attr].split()
+                for i, new_node in enumerate(nodes):
+                    if values[i] == '*':
+                        setattr(new_node, attr, orig_attr[attr])
+                    else:
+                        setattr(new_node, attr, values[i])
+
+        mwt = node.root.create_multiword_token(nodes, orig_attr['form'], orig_attr['misc'])
+        node.misc = None
+        self.postprocess_mwt(mwt)
+
+    def multiword_analysis(self, node):
+        """Return a dict with MWT info or None if `node` does not represent a multiword token.
+
+        An example return value is::
+        {
+            'form': 'aby bych',
+            'lemma': 'aby být',
+            'upos': 'SCONJ AUX',
+            'xpos': 'J,------------- Vc-S---1-------',
+            'feats': '_ Mood=Cnd|Number=Sing|Person=1|VerbForm=Fin', # _ means empty FEATS
+            'deprel': '* aux', # * means keep the original deprel
+            'main': 0, # which of the two words will inherit the original children (if any)
+            'shape': 'siblings', # the newly created nodes will be siblings or alternatively
+            #'shape': 'subtree', # the main-indexed node will be the head
+        }
+        """
+        raise NotImplementedError('multiword_analysis must be overriden in subclasses')
+
+    def postprocess_mwt(self, mwt):
+        """Optional postprocessing of newly created MWTs."""
+        pass
---
+++ udapi/block/ud/cs/addmwt.py
@@ -0,0 +1,74 @@
+"""Block ud.cs.AddMwt for heuristic detection of multi-word tokens."""
+import udapi.block.ud.addmwt
+
+MWTS = {
+    'abych': {'form': 'aby bych', 'feats': '_ Mood=Cnd|Number=Sing|Person=1|VerbForm=Fin'},
+    'kdybych': {'form': 'když bych', 'feats': '_ Mood=Cnd|Number=Sing|Person=1|VerbForm=Fin'},
+    'abys': {'form': 'aby bys', 'feats': '_ Mood=Cnd|Number=Sing|Person=2|VerbForm=Fin'},
+    'kdybys': {'form': 'když bys', 'feats': '_ Mood=Cnd|Number=Sing|Person=2|VerbForm=Fin'},
+    'aby': {'form': 'aby by', 'feats': '_ Mood=Cnd|Person=3|VerbForm=Fin'},
+    'kdyby': {'form': 'když by', 'feats': '_ Mood=Cnd|Person=3|VerbForm=Fin'},
+    'abychom': {'form': 'aby bychom', 'feats': '_ Mood=Cnd|Number=Plur|Person=1|VerbForm=Fin'},
+    'kdybychom': {'form': 'když bychom', 'feats': '_ Mood=Cnd|Number=Plur|Person=1|VerbForm=Fin'},
+    'abyste': {'form': 'aby byste', 'feats': '_ Mood=Cnd|Number=Plur|Person=2|VerbForm=Fin'},
+    'kdybyste': {'form': 'když byste', 'feats': '_ Mood=Cnd|Number=Plur|Person=2|VerbForm=Fin'},
+}
+for v in MWTS.values():
+    v['upos'] = 'SCONJ AUX'
+    number = '-'
+    if 'Sing' in v['feats']:
+        number = 'S'
+    elif 'Plur' in v['feats']:
+        number = 'P'
+    person = '-'
+    if 'Person=1' in v['feats']:
+        person = '1'
+    elif 'Person=2' in v['feats']:
+        person = '2'
+
+    v['xpos'] = 'J,------------- Vc-%s---%s-------' % (number, person)
+    v['deprel'] = '* aux'
+    v['lemma'] = v['form'].split()[0] + ' být'
+    v['main'] = 0
+    v['shape'] = 'siblings'
+
+# nač -> na + co
+for prep in 'na za o'.split():
+    MWTS[prep + 'č'] = {
+        'form': prep + ' co',
+        'lemma': prep + ' co',
+        'upos': 'ADP PRON',
+        'deprel': 'case *',
+        'main': 1,
+        'shape': 'subtree',
+    }
+
+class AddMwt(udapi.block.ud.addmwt.AddMwt):
+    """Detect and mark MWTs (split them into words and add the words to the tree)."""
+
+    def multiword_analysis(self, node):
+        """Return a dict with MWT info or None if `node` does not represent a multiword token."""
+        analysis = MWTS.get(node.form.lower(), None)
+        if analysis is not None:
+            return analysis
+
+        # There is no VerbType=verbconj in the UD_Czech data.
+        # The purpose of this rule is rather to show that
+        # it is possible to write such "dynamic" rules
+        # (which cannot be included in static MWTS).
+        if node.form.lower().endswith('ť') and node.feats['VerbType'] == 'verbconj':
+            return {
+                'form': node.form.lower()[:-1] + ' neboť',
+                'lemma': '* neboť',
+                'upos': '* CCONJ',
+                'xpos': 'Vt-S---3P-NA--2 J^-------------',
+                'feats': '* _',
+                'deprel': '* cc',
+                'main': 0,
+                'shape': 'subtree',
+            }
+        return None
+
+    def postprocess_mwt(self, mwt):
+        if mwt.words[0].deprel == 'fixed' and mwt.words[0].parent.parent.upos == 'VERB':
+            mwt.words[1].parent = mwt.words[0].parent.parent
