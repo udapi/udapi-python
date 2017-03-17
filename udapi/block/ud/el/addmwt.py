5b814e651d820361599da28cb75d19e179c5b2b4
Martin Popel <popel@ufal.mff.cuni.cz>
2017-03-17 15:17:36 +0100

    simpify ud.el.AddMwt by the newly introduced Gender=*

--- udapi/block/ud/el/addmwt.py
+++ udapi/block/ud/el/addmwt.py
@@ -2,47 +2,26 @@
 import udapi.block.ud.addmwt
 
 MWTS = {
-    'στη': {'form'  : 'σ τη', 
-            'lemma' : 'σε ο', 
-            'upos'  : 'ADP DET',  
+    'στη': {'form'  : 'σ τη',
+            'lemma' : 'σε ο',
+            'upos'  : 'ADP DET',
             'xpos'  : 'AsPpSp AtDf',
-            'feats' : '_ Case=Acc|Gender=Fem|Number=Sing', 
-            'deprel': 'case det', 
+            'feats' : '_ Case=Acc|Gender=Fem|Number=Sing',
+            'deprel': 'case det',
             'main'  : 0, # which of the two words will inherit the original children (if any)
-            'shape' : 'siblings', # the newly created nodes will be siblings},	
+            'shape' : 'siblings', # the newly created nodes will be siblings},
             },
-    'στην':  {'form': 'σ την',  'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Fem|Number=Sing',  'deprel': 'case det', 'main': 0,  'shape': 'siblings'},   
-    'στις':  {'form': 'σ τις',  'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Fem|Number=Plur',  'deprel': 'case det', 'main': 0,  'shape': 'siblings'},   
+    'στην':  {'form': 'σ την',  'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Fem|Number=Sing',  'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
+    'στις':  {'form': 'σ τις',  'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Fem|Number=Plur',  'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
     'στα':   {'form': 'σ τα',   'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Neut|Number=Plur', 'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
     'στους': {'form': 'σ τους', 'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Masc|Number=Plur', 'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
     'στον':  {'form': 'σ τον',  'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=Masc|Number=Sing', 'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
+    'στο':   {'form': 'σ το',   'lemma': 'σε ο',  'upos':'ADP DET', 'xpos':'AsPpSp AtDf', 'feats': '_ Case=Acc|Gender=*|Number=Sing',    'deprel': 'case det', 'main': 0,  'shape': 'siblings'},
 }
-
-#for v in MWTS.values():
-#    pass
 
 class AddMwt(udapi.block.ud.addmwt.AddMwt):
     """Detect and mark MWTs (split them into words and add the words to the tree)."""
 
     def multiword_analysis(self, node):
         """Return a dict with MWT info or None if `node` does not represent a multiword token."""
-        analysis = MWTS.get(node.form.lower(), None)
-        if analysis is not None:
-            return analysis
-
-        # Write a rule for ambigous prep+article MWTs
-        if node.form.lower() == 'στο' and node.feats['Gender'] == 'Masc':
-            return {
-                'form': 'σ το','lemma': 'σε ο','upos': 'ADP DET','xpos': 'AsPpSp AtDf','deprel': 'case det','main': 0,'shape': 'siblings',
-                'feats': '_ Case=Acc|Gender=Masc|Number=Sing',
-            }
-        elif node.form.lower() == 'στο':
-            return {
-                'form': 'σ το','lemma': 'σε ο','upos': 'ADP DET','xpos': 'AsPpSp AtDf','deprel': 'case det','main': 0,'shape': 'siblings',
-                'feats': '_ Case=Acc|Gender=Neut|Number=Sing',
-            }
-            
-        return None
-
-#    def postprocess_mwt(self, mwt):
-#        pass
+        return MWTS.get(node.form.lower(), None)
