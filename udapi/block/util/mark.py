d334291bdea48439f7f5d5956bce31f2be9133ed
Martin Popel <popel@ufal.mff.cuni.cz>
2017-04-27 17:49:10 +0200

    util.Mark add=0 causes all previously marked nodes to get unmarked

--- udapi/block/util/mark.py
+++ udapi/block/util/mark.py
@@ -15,7 +15,7 @@ class Mark(Block):
     udapy -TM util.Mark node='node.is_nonprojective()' < in | less -R
     """
 
-    def __init__(self, node, mark=1, **kwargs):
+    def __init__(self, node, mark=1, add=True, **kwargs):
         """Create the Mark block object.
 
         Args:
@@ -23,11 +23,18 @@ class Mark(Block):
             the node will be marked.
 
         `mark`: the node will be marked with `Mark=<mark>` in `node.misc`. Default=1.
+
+        `add`: should we keep existing Mark|ToDo|Bug? Default=True.
         """
         super().__init__(**kwargs)
         self.mark = mark
         self.node = node
+        self.add = add
 
     def process_node(self, node):
         if eval(self.node):
             node.misc['Mark'] = self.mark
+        elif not self.add:
+            del node.misc['Mark']
+            del node.misc['ToDo']
+            del node.misc['Bug']
