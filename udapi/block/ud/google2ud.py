f21be2aa1c1c3f65ab36dc0bcda4f95549ea8db4
Martin Popel <popel@ufal.mff.cuni.cz>
2017-04-30 02:27:52 +0200

    new ud.fr.AddMwt, used in ud.Google2ud

--- udapi/block/ud/google2ud.py
+++ udapi/block/ud/google2ud.py
@@ -9,6 +9,7 @@ from udapi.block.ud.fixchain import FixChain
 from udapi.block.ud.fixrightheaded import FixRightheaded
 from udapi.block.ud.fixpunct import FixPunct
 from udapi.block.ud.de.addmwt import AddMwt as de_AddMwt
+from udapi.block.ud.fr.addmwt import AddMwt as fr_AddMwt
 from udapi.block.ud.pt.addmwt import AddMwt as pt_AddMwt
 
 DEPREL_CHANGE = {
@@ -123,6 +124,8 @@ class Google2ud(Convert1to2):
         self._addmwt_block = None
         if lang == 'de':
             self._addmwt_block = de_AddMwt()
+        elif lang == 'fr':
+            self._addmwt_block = fr_AddMwt()
         elif lang == 'pt':
             self._addmwt_block = pt_AddMwt()
 
