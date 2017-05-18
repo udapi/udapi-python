33bdb7d7529f7b07870927155bde15fc64852240
Martin Popel <popel@ufal.mff.cuni.cz>
2017-05-18 23:23:28 +0200

    FixChain should fix also deprel=flat

--- udapi/block/ud/fixchain.py
+++ udapi/block/ud/fixchain.py
@@ -1,11 +1,11 @@
-"""Block ud.FixChain for making sure deprel=fixed|goeswith|list does not form a chain."""
+"""Block ud.FixChain for making sure deprel=fixed|flat|goeswith|list does not form a chain."""
 from udapi.core.block import Block
 
 
 class FixChain(Block):
-    """Make sure deprel=fixed does not form a chain, but a flat structure."""
+    """Make sure deprel=fixed etc. does not form a chain, but a flat structure."""
 
-    def __init__(self, deprels='fixed,goeswith,list', **kwargs):
+    def __init__(self, deprels='fixed,flat,goeswith,list', **kwargs):
         """Args:
         deprels: comma-separated list of deprels to be fixed. Default = fixed,goeswith,list.
         """
