3fe163a4c0feb5e844e325024b5f6e6da66c5764
Martin Popel <popel@ufal.mff.cuni.cz>
2017-06-26 23:42:16 +0200

    ud.JoinAsMwt: don't duplicate MISC in MWTs, param revert_orig_form

--- udapi/block/ud/joinasmwt.py
+++ udapi/block/ud/joinasmwt.py
@@ -9,6 +9,15 @@ from udapi.core.block import Block
 class JoinAsMwt(Block):
     """Create MWTs if words are not separated by a space.."""
 
+    def __init__(self, revert_orig_form=True, **kwargs):
+        """Args:
+        revert_orig_form: if any node of the newly created MWT has `misc['OrigForm']`,
+            it is used as the FORM (and deleted from MISC). Useful after `ud.ComplyWithText`.
+            Default=True.
+        """
+        super().__init__(**kwargs)
+        self.revert_orig_form = revert_orig_form
+
     def process_node(self, node):
         if node.multiword_token:
             return
@@ -19,6 +28,13 @@ class JoinAsMwt(Block):
             mwt_nodes.append(node)
         if len(mwt_nodes) > 1:
             mwt_form = ''.join([n.form for n in mwt_nodes])
-            node.root.create_multiword_token(mwt_nodes, mwt_form, node.misc)
+            mwt = node.root.create_multiword_token(mwt_nodes, mwt_form)
+            if node.misc['SpaceAfter'] == 'No':
+                mwt.misc['SpaceAfter'] = 'No'
             for mwt_node in mwt_nodes:
                 del mwt_node.misc['SpaceAfter']
+            if self.revert_orig_form:
+                for mwt_node in mwt_nodes:
+                    if mwt_node.misc['OrigForm']:
+                        mwt_node.form = mwt_node.misc['OrigForm']
+                        del mwt_node.misc['OrigForm']
