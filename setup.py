31c87219f7976f571c1e6fc8161cb3fd912fb38b
Martin Popel <popel@ufal.mff.cuni.cz>
2017-10-23 20:41:46 +0200

    update setup.py
    * rename the distribution to just "udapi"
     (the original suffix "-python" on PyPI makes no sense)
    * bump version to 0.2.1
     (semver.org requires X.Y.Z where Z cannot be omitted)
    * exclude ufal.udpipe from install_requires, but keep it in requirements.txt
      - https://packaging.python.org/discussions/install-requires-vs-requirements/ says
      "install_requires [...] should be used to specify what a project minimally needs"
      while requirements.txt often contains an exhaustive listing.
      - readthedocs.io has problems with ufal.udpipe because of missing C++11
    * add fields required by "setup.py sdist"

--- setup.py
+++ setup.py
@@ -9,15 +9,25 @@ if sys.version_info < (3, 3):
     raise SystemExit('Udapi requires Python 3.3 or higher.')
 
 setup(
-    name='udapi-python',
-    version='0.2',
+    name='udapi',
+    version='0.2.1',
     description='Python framework for processing Universal Dependencies data',
+    long_description=(
+      'Udapi is an open-source framework providing API for processing '
+      'Universal Dependencies data. It is available in Python, Perl and Java. '
+      'Udapi is suitable both for full-fledged applications and fast '
+      'prototyping: visualization of dependency trees, format conversions, '
+      'querying, editing and transformations, validity tests, dependency '
+      'parsing, evaluation etc.'
+    ),
     author='Martin Popel',
     author_email='popel@ufal.mff.cuni.cz',
     url='https://github.com/udapi/udapi-python',
     packages=find_packages(),
     scripts=['bin/udapy'],
     tests_require=['pytest'],
-    install_requires=['colorama', 'termcolor', 'ufal.udpipe'],
+    install_requires=['colorama', 'termcolor'],
     python_requires='>=3.3',
+    license='GPL 2 or newer',
+    platforms='any',
 )
