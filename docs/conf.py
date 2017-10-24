555c9c4b77eaedd3984f284d3d1b59f10f1e1aca
Martin Popel <popel@ufal.mff.cuni.cz>
2017-10-24 18:38:41 +0200

    mock for ufal.udpipe is not needed
    It did not solve the problem anyway.

--- docs/conf.py
+++ docs/conf.py
@@ -21,16 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['ufal.udpipe']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
