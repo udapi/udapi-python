c8fbca0a38e8ce741f5fda4a9b3778f186ce41a1
Martin Popel <popel@ufal.mff.cuni.cz>
2018-04-03 19:19:35 +0200

    add language param for AddCommas, add RemoveCommas

--- udapi/block/tutorial/addcommas.py
+++ udapi/block/tutorial/addcommas.py
@@ -1,9 +1,22 @@
 """tutorial.AddCommas block template."""
 from udapi.core.block import Block
 
+# nickname = xy123
+# TODO: make up a unique nickname and edit the previous line
+# if you want your results to be listed on the NPFL070 web (under that nickname).
+# Delete the line if you don't want to listed on the web.
 
 class AddCommas(Block):
     """Heuristically insert nodes for missing commas."""
+
+    def __init__(self, language='en', **kwargs):
+        """Create the AddCommas block object.
+
+        Args:
+        `language`: which language-specific rules to use ('en' or 'cs')
+        """
+        super().__init__(**kwargs)
+        self.language = language
 
     def process_node(self, node):
         if self.should_add_comma_before(node):
@@ -15,7 +28,7 @@ class AddCommas(Block):
         prev_node = node.prev_node
         if prev_node is None:
             return False
-        if prev_node.lemma == 'however':
+        if self.language == 'en' and node.lemma == 'however':
             return True
         if any(n.deprel == 'appos' for n in prev_node.children):
             return True
