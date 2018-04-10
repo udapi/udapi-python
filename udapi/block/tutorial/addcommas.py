984a9ea5c1f98ee5aba2fea4e762bd54b953f213
Martin Popel <popel@ufal.mff.cuni.cz>
2018-04-10 17:55:01 +0200

    fix bug (prev_node instead of node) introduced in my last commit

--- udapi/block/tutorial/addcommas.py
+++ udapi/block/tutorial/addcommas.py
@@ -10,25 +10,20 @@ class AddCommas(Block):
     """Heuristically insert nodes for missing commas."""
 
     def __init__(self, language='en', **kwargs):
-        """Create the AddCommas block object.
-
-        Args:
-        `language`: which language-specific rules to use ('en' or 'cs')
-        """
         super().__init__(**kwargs)
         self.language = language
 
     def process_node(self, node):
+        # TODO: Your task: implement some heuristics
         if self.should_add_comma_before(node):
             comma = node.create_child(form=',', deprel='punct', upos='PUNCT')
             comma.shift_before_node(node)
 
     def should_add_comma_before(self, node):
-        # TODO: Your task: implement some heuristics
         prev_node = node.prev_node
         if prev_node is None:
             return False
-        if self.language == 'en' and node.lemma == 'however':
+        if self.language == 'en' and prev_node.lemma == 'however':
             return True
         if any(n.deprel == 'appos' for n in prev_node.children):
             return True
