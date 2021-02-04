156b66ed2903757a2ffa51944be65713a68f27ab
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-04 22:03:36 +0100

    create_mention improvements - If no head is specified, the first word from mention_words will be used instead. - If mention_words are provided, they must contain the head. - docstring

--- udapi/core/coref.py
+++ udapi/core/coref.py
@@ -45,7 +45,7 @@ class CorefMention(object):
             raise ValueError(f"Head {self.head} not in new_words {new_words}")
         for old_word in self._words:
             old_word._mentions.remove(self)
-        self._words = new_words
+        self._words = new_words # TODO sorted
         for new_word in new_words:
             new_word._mentions.append(self)
 
@@ -63,7 +63,7 @@ class CorefMention(object):
 
         if not self._words:
             return ''
-        ords = sorted(n.ord for n in self._words)
+        ords = sorted(n.ord for n in self._words) # TODO vypustit sorted
         if len(ords) == 1:
             return str(ords[0])
         first, last = ords[0], ords[-1]
@@ -73,24 +73,7 @@ class CorefMention(object):
 
     @span.setter
     def span(self, new_span):
-        ranges = []
-        for span_str in new_span.split(','):
-            if '-' not in span_str:
-                lo = hi = float(span_str)
-            else:
-                lo, hi = (float(x) for x in span_str.split('-'))
-            ranges.append((lo, hi))
-
-        def _num_in_ranges(num):
-            for (lo, hi) in ranges:
-                if num > hi:
-                    return False
-                if num >= lo:
-                    return True
-                return False
-
-        new_words = [w for w in self._head.root.descendants_and_empty if _num_in_ranges(w.ord)]
-        self.words = new_words
+        self.words = span_to_nodes(self._head.root, new_span)
 
 
 class CorefCluster(object):
@@ -109,15 +92,41 @@ class CorefCluster(object):
 
     @property
     def mentions(self):
+        #TODO return sorted(self._mentions, key=lambda x:...
         return self._mentions
 
-    def create_mention(self, head, mention_words=None, mention_span=None):
-        if mention_words is not None and mention_span is not None:
-            raise ValueError("Cannot set both mention_words and mention_span")
+    def create_mention(self, head=None, mention_words=None, mention_span=None):
+        """Create a new CoreferenceMention object within this CorefCluster.
+
+        Args:
+        head: a node where the annotation about this CorefMention will be stored in MISC.
+            The head is supposed to be the linguistic head of the mention,
+            i.e. the highest node in the dependency tree,
+            but if such information is not available (yet),
+            it can be any node within the mention_words.
+            If no head is specified, the first word from mention_words will be used instead.
+        mention_words: a list of nodes of the mention.
+            This argument is optional, but if provided, it must contain the head.
+            The nodes can be both normal nodes or empty nodes.
+        mention_span: an alternative way how to specify mention_words
+            using a string such as "3-5,6,7.1-7.2".
+            (which means, there is an empty node 5.1 and normal node 7,
+            which are not part of the mention).
+            At most one of the args mention_words and mention_span can be specified.
+        """
+        if mention_words and mention_span:
+            raise ValueError("Cannot specify both mention_words and mention_span")
+        if head and mention_words and head not in mention_words:
+            raise ValueError(f"Head {head} is not among the specified mention_words")
+        if head is None and mention_words is None:
+            raise ValueError("Either head or mention_words must be specified")
+        if head is None:
+            head = mention_words[0]
+
         mention = CorefMention(head, self)
-        if mention_words is not None:
+        if mention_words:
             mention.words = mention_words
-        elif mention_span is not None:
+        if mention_span:
             mention.span = mention_span
         return mention
 
@@ -172,8 +181,7 @@ def load_coref_from_misc(doc):
             mention._split_ante = node.misc["Split"]
     doc._coref_clusters = clusters
 
-# TODO don't recompute the serialization if not needed, i.e. if doc._is_coref_stored_in_misc is True,
-# but make it False after each edit of coref or change of ord (reordering, new nodes, deleted nodes...).
+
 def store_coref_to_misc(doc):
     if not doc._coref_clusters:
         return
@@ -191,3 +199,43 @@ def store_coref_to_misc(doc):
             head.misc["ClusterType"] = cluster.cluster_type
             head.misc["Bridging"] = mention.bridging
             head.misc["Split"] = cluster.split_ante
+
+
+def span_to_nodes(root, span):
+    ranges = []
+    for span_str in span.split(','):
+        if '-' not in span_str:
+            lo = hi = float(span_str)
+        else:
+            lo, hi = (float(x) for x in span_str.split('-'))
+        ranges.append((lo, hi))
+
+    def _num_in_ranges(num):
+        for (lo, hi) in ranges:
+            if num > hi:
+                return False
+            if num >= lo:
+                return True
+            return False
+
+    return [w for w in root.descendants_and_empty if _num_in_ranges(w.ord)]
+
+
+def nodes_to_span(nodes):
+    def _nums_to_ranges(nums):
+        lo, hi = nums[0], nums[0]
+        for num in nums[1:]:
+            if num == hi + 1:
+                hi = num
+            else:
+                yield (lo, hi)
+                lo, hi = num, num
+        yield (lo, hi)
+
+    ords = sorted(n.ord for n in nodes) # TODO vypustit sorted
+    if len(ords) == 1:
+        return str(ords[0])
+    first, last = ords[0], ords[-1]
+    if ords == list(range(first, last+1)):
+        return "%g-%g" % (first, last)
+    return ','.join( '%g' % r[0] if r[0]==r[1] else '%g-%g' % r for r in _nums_to_ranges(ords))
