76ba743616125d84f48c63443eb5fdbe7d680861
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-06 19:02:01 +0100

    support for ClusterId[1]
    reading+writing support for multiple clusters in one node
    stored using layered attributes

--- udapi/core/coref.py
+++ udapi/core/coref.py
@@ -1,4 +1,5 @@
 """Classes for handling coreference."""
+import re
 
 class CorefMention(object):
     """Class for representing a mention (instance of an entity)."""
@@ -148,43 +149,62 @@ def create_coref_cluster(head, cluster_id=None, cluster_type=None, **kwargs):
 def load_coref_from_misc(doc):
     clusters = {}
     for node in doc.nodes:
+        index, index_str = 0, ""
         cluster_id = node.misc["ClusterId"]
-        if cluster_id:
+        if not cluster_id:
+            index, index_str = 1, "[1]"
+            cluster_id = node.misc["ClusterId[1]"]
+        while cluster_id:
             cluster = clusters.get(cluster_id)
             if cluster is None:
                 cluster = CorefCluster(cluster_id)
                 clusters[cluster_id] = cluster
             mention = CorefMention(node, cluster)
-            if node.misc["MentionSpan"]:
-                mention.span = node.misc["MentionSpan"]
-            cluster_type = node.misc["ClusterType"]
+            if node.misc["MentionSpan" + index_str]:
+                mention.span = node.misc["MentionSpan" + index_str]
+            cluster_type = node.misc["ClusterType" + index_str]
             if cluster_type is not None:
                 if cluster.cluster_type is not None and cluster_type != cluster.cluster_type:
                     logging.warning(f"cluster_type mismatch in {node}: {cluster.cluster_type} != {cluster_type}")
                 cluster.cluster_type = cluster_type
             # TODO deserialize Bridging and SplitAnte
-            mention._bridging = node.misc["Bridging"]
-            cluster._split_ante = node.misc["SplitAnte"]
+            mention._bridging = node.misc["Bridging" + index_str]
+            cluster._split_ante = node.misc["SplitAnte" + index_str]
+            index += 1
+            index_str = f"[{index}]"
+            cluster_id = node.misc["ClusterId" + index_str]
     doc._coref_clusters = clusters
 
 
 def store_coref_to_misc(doc):
     if not doc._coref_clusters:
         return
+    attrs = ("ClusterId", "MentionSpan", "ClusterType", "Bridging", "SplitAnte")
     for node in doc.nodes:
-        del node.misc["ClusterId"]
-        del node.misc["MentionSpan"]
-        del node.misc["ClusterType"]
-        del node.misc["Bridging"]
-        del node.misc["SplitAnte"]
+        for key in list(node.misc):
+            if any(re.match(attr + r'(\[\d+\])?$', key) for attr in attrs):
+                del node.misc[key]
     for cluster in doc._coref_clusters.values():
         for mention in cluster.mentions:
             head = mention.head
-            head.misc["ClusterId"] = cluster.cluster_id
-            head.misc["MentionSpan"] = mention.span
-            head.misc["ClusterType"] = cluster.cluster_type
-            head.misc["Bridging"] = mention.bridging
-            head.misc["SplitAnte"] = cluster.split_ante
+            if head.misc["ClusterId"]:
+                for a in attrs:
+                    if head.misc[a]:
+                        head.misc[a + "[1]"] = head.misc[a]
+                        del head.misc[a]
+                index_str = "[2]"
+            else:
+                index, index_str = 1, "[1]"
+                while(head.misc["ClusterId" + index_str]):
+                    index += 1
+                    index_str = f"[{index}]"
+                if index == 1:
+                    index_str = ""
+            head.misc["ClusterId" + index_str] = cluster.cluster_id
+            head.misc["MentionSpan" + index_str] = mention.span
+            head.misc["ClusterType" + index_str] = cluster.cluster_type
+            head.misc["Bridging" + index_str] = mention.bridging
+            head.misc["SplitAnte" + index_str] = cluster.split_ante
 
 
 def span_to_nodes(root, span):
