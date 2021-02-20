d466683c947b18b5b9dd7b39f584a15a0e44b558
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-20 04:12:44 +0100

    util.Wc tsv=1 for easier-to-parse output

--- udapi/block/util/wc.py
+++ udapi/block/util/wc.py
@@ -5,10 +5,15 @@ from udapi.core.block import Block
 class Wc(Block):
     """Special block for printing statistics (word count etc)."""
 
-    def __init__(self, **kwargs):
-        """Create the Wc block object."""
+    def __init__(self, tsv=False, **kwargs):
+        """Create the Wc block object.
+
+        Params:
+        tsv: print just tab-separated-values (trees, words, tokens, MWTs, empty nodes)
+        """
         super().__init__(**kwargs)
         self.trees, self.words, self.mwts, self.tokens, self.empty = 0, 0, 0, 0, 0
+        self.tsv = tsv
 
     def process_tree(self, tree):
         self.trees += 1
@@ -19,8 +24,11 @@ class Wc(Block):
         self.empty += len(tree.empty_nodes)
 
     def process_end(self):
-        print('%8d trees\n%8d words' % (self.trees, self.words))
-        if self.mwts:
-            print('%8d multi-word tokens\n%8d tokens' % (self.mwts, self.tokens))
-        if self.empty:
-            print('%8d empty nodes' % self.empty)
+        if self.tsv:
+            print('\t'.join(map(str, (self.trees, self.words, self.tokens, self.mwts, self.empty))))
+        else:
+            print('%8d trees\n%8d words' % (self.trees, self.words))
+            if self.mwts:
+                print('%8d multi-word tokens\n%8d tokens' % (self.mwts, self.tokens))
+            if self.empty:
+                print('%8d empty nodes' % self.empty)
