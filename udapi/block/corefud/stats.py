9161cbdec054700d6cfbb28664df720038b03363
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-24 15:58:45 +0100

    redesign corefud.Stats

--- udapi/block/corefud/stats.py
+++ udapi/block/corefud/stats.py
@@ -4,11 +4,18 @@ from collections import Counter
 class Stats(Block):
     """Block corefud.Stats prints various coreference-related statistics."""
 
-    def __init__(self, m_len_max=5, c_len_max=5, focus='non-singletons', **kwargs):
+    def __init__(self, m_len_max=5, c_len_max=5, report_mentions=True, report_clusters=True,
+                 exclude_singletons=False, exclude_nonsingletons=False, style='human', **kwargs):
         super().__init__(**kwargs)
         self.m_len_max = m_len_max
         self.c_len_max = c_len_max
-        self.focus = focus
+        self.report_mentions = report_mentions
+        self.report_clusters = report_clusters
+        self.exclude_singletons = exclude_singletons
+        self.exclude_nonsingletons = exclude_nonsingletons
+        self.style = style
+        if style not in 'tex human'.split():
+            raise ValueError(f'Unknown style f{style}')
 
         self.counter = Counter()
         self.mentions = 0
@@ -23,13 +30,12 @@ class Stats(Block):
         self.total_nodes += len(list(doc.nodes))
         for cluster in doc.coref_clusters.values():
             len_mentions = len(cluster.mentions)
+            if len_mentions == 1 and self.exclude_singletons:
+                continue
+            elif len_mentions > 1 and self.exclude_nonsingletons:
+                continue
             self.longest_cluster = max(len_mentions, self.longest_cluster)
             self.counter[f"c_len_{min(len_mentions, self.c_len_max)}"] += 1
-            if len_mentions == 1:
-                if self.focus.startswith('non-singletons'):
-                    continue
-            elif self.focus.startswith('singletons'):
-                continue
 
             self.clusters += 1
             for mention in cluster.mentions:
@@ -42,17 +48,28 @@ class Stats(Block):
                 self.counter[f"m_len_{min(non_empty, self.m_len_max)}"] += 1
 
     def process_end(self):
-        mentions = 1 if self.mentions == 0 else self.mentions
-        clusters = 1 if self.clusters == 0 else self.clusters
-        total_nodes = 1 if self.total_nodes == 0 else self.total_nodes
+        mentions_nonzero = 1 if self.mentions == 0 else self.mentions
+        clusters_nonzero = 1 if self.clusters == 0 else self.clusters
+        total_nodes_nonzero = 1 if self.total_nodes == 0 else self.total_nodes
 
-        if 'clusters' in self.focus:
-            clusters_per1k = 1000 * self.clusters / total_nodes
-            percents = [100 * self.counter[f"c_len_{i}"] / clusters for i in range(1, self.c_len_max + 1)]
-            print(f"{self.clusters:7,} & {clusters_per1k:6.0f} & {self.longest_cluster:6} & "
-                + " & ".join(f"{p:5.1f}" for p in percents) + r" \\")
-        if self.focus != 'clusters':
-            mentions_per1k = 1000 * self.mentions / total_nodes
-            percents = [100 * self.counter[f"m_len_{i}"] / mentions for i in range(self.m_len_max + 1)]
-            print(f"{self.mentions:7,} & {mentions_per1k:6.0f} & {self.longest_mention:6} & "
-                + " & ".join(f"{p:5.1f}" for p in percents) + r" \\")
+        columns =[ ]
+        if self.report_clusters:
+            columns += [('clusters', f"{self.clusters:7,}"),
+                        ('clusters_per1k', f"{1000 * self.clusters / total_nodes_nonzero:6.0f}"),
+                        ('longest_cluster', f"{self.longest_cluster:6}")]
+            for i in range(1, self.c_len_max + 1):
+                percent = 100 * self.counter[f"c_len_{i}"] / clusters_nonzero
+                columns.append((f"c_len_{i}{'' if i < self.c_len_max else '+'}", f"{percent:5.1f}"))
+        if self.report_mentions:
+            columns += [('mentions', f"{self.mentions:7,}"),
+                        ('mentions_per1k', f"{1000 * self.mentions / total_nodes_nonzero:6.0f}"),
+                        ('longest_mention', f"{self.longest_mention:6}")]
+            for i in range(0, self.m_len_max + 1):
+                percent = 100 * self.counter[f"m_len_{i}"] / mentions_nonzero
+                columns.append((f"m_len_{i}{'' if i < self.m_len_max else '+'}", f"{percent:5.1f}"))
+
+        if self.style == 'tex':
+            print(" & ".join(c[1] for c in columns))
+        elif self.style == 'human':
+            for c in columns:
+                print(f"{c[0]:>15} = {c[1].strip():>10}")
