a69ec13d9becefa7cffc8dbf4a2d3e1dc33db209
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-24 01:07:47 +0100

    improved coref stats

--- udapi/block/corefud/stats.py
+++ udapi/block/corefud/stats.py
@@ -1,33 +1,46 @@
 from udapi.core.block import Block
 from collections import Counter
-import pprint
 
 class Stats(Block):
     """Block corefud.Stats prints various coreference-related statistics."""
 
-    def __init__(self, m_len_max=5, **kwargs):
+    def __init__(self, m_len_max=5, focus='non-singletons', **kwargs):
         super().__init__(**kwargs)
         self.m_len_max = m_len_max
+        self.focus = focus
 
         self.counter = Counter()
         self.mentions = 0
         self.clusters = 0
-        self.nodes = 0
+        self.total_nodes = 0
         self.longest_mention = 0
+        self.m_words = 0
+        self.m_empty = 0
 
     def process_document(self, doc):
-        self.nodes += len(list(doc.nodes))
+        self.total_nodes += len(list(doc.nodes))
         for cluster in doc.coref_clusters.values():
+            if len(cluster.mentions) == 1:
+                if self.focus == 'non-singletons':
+                    continue
+            elif self.focus == 'singletons':
+                continue
+
             self.clusters += 1
             for mention in cluster.mentions:
                 self.mentions += 1
-                words = len(mention.words)
-                self.longest_mention = max(words, self.longest_mention)
-                self.counter[f"m_len_{min(words, self.m_len_max)}"] += 1
+                all_words = len(mention.words)
+                non_empty = len([w for w in mention.words if not w.is_empty()])
+                self.m_words += all_words
+                self.m_empty += all_words - non_empty
+                self.longest_mention = max(non_empty, self.longest_mention)
+                self.counter[f"m_len_{min(non_empty, self.m_len_max)}"] += 1
 
     def process_end(self):
-        #pprint.pprint(self.counter)
-        mentions_per1k = 1000 * self.mentions / self.nodes
-        percents = [100 * self.counter[f"m_len_{i}"]/self.mentions for i in range(self.m_len_max + 1)]
-        print(f"{self.mentions:6} & {mentions_per1k:6.0f} & {self.longest_mention:6} & "
-              + " & ".join(f"{p:3.0f}" for p in percents) + r" \\")
+        mentions = 1 if self.mentions == 0 else self.mentions
+        total_nodes = 1 if self.total_nodes == 0 else self.total_nodes
+        mentions_per1k = 1000 * self.mentions / total_nodes
+
+        percents = [100 * self.counter[f"m_len_{i}"] / mentions for i in range(self.m_len_max + 1)]
+        print(f"{self.mentions:7,} & {mentions_per1k:6.0f} & {self.longest_mention:6} & "
+              + " & ".join(f"{p:5.1f}" for p in percents) + r" \\")
