c627846c40fc8f99cf68cb91662bd7b512b6dfd2
Zdenek Zabokrtsky <zabokrtsky@ufal.mff.cuni.cz>
2021-02-27 23:56:55 +0100

    a block for concatenating all MISC attributes named MentionMisc_... into a single value of MentionMisc

---
+++ udapi/block/corefud/concatmentionmisc.py
@@ -0,0 +1,23 @@
+from udapi.core.block import Block
+from collections import Counter
+import re
+
+class ConcatMentionMisc(Block):
+    """All MISC attributes named MentionMisc_... are concatenated into MentionMisc"""
+
+    def process_node(self,node):
+        for attrname in list(node.misc):
+            matchObj = re.match('MentionMisc_([^[]+)((\[\d+\])?)',attrname)
+            if matchObj:
+                innerattrib = matchObj.group(1)
+                index = matchObj.group(2)
+
+                finalattr = 'MentionMisc'+index
+                value = node.misc[attrname]
+                
+                if finalattr not in node.misc:
+                    node.misc[finalattr] = f'{innerattrib}:{value}'
+                else:
+                    node.misc[finalattr] += f' {innerattrib}:{value}'
+                del node.misc[attrname]
+
