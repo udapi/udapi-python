5fe2ab7829f8d2b5895ca714fb5312925e0655ab
Martin Popel <popel@ufal.mff.cuni.cz>
2021-02-28 21:41:50 +0100

    `write.Conllu overwrite=1` will overwrite the input files
    as stored in `doc.meta["loaded_from"]`

--- udapi/core/basewriter.py
+++ udapi/core/basewriter.py
@@ -11,7 +11,7 @@ class BaseWriter(Block):
     """Base class for all reader blocks."""
 
     def __init__(self, files='-', filehandle=None, docname_as_file=False, encoding='utf-8',
-                 newline='\n', **kwargs):
+                 newline='\n', overwrite=False, **kwargs):
         super().__init__(**kwargs)
         self.orig_files = files
         self.orig_stdout = sys.stdout
@@ -24,6 +24,11 @@ class BaseWriter(Block):
         self.docname_as_file = docname_as_file
         if docname_as_file and files != '-':
             raise ValueError("docname_as_file=1 is not compatible with files=" + files)
+        self.overwrite = overwrite
+        if overwrite and files != '-':
+            raise ValueError("overwrite=1 is not compatible with files=" + files)
+        if overwrite and docname_as_file:
+            raise ValueError("overwrite=1 is not compatible with docname_as_file=1")
 
     @property
     def filename(self):
@@ -54,6 +59,13 @@ class BaseWriter(Block):
                     sys.stdout = open(docname, 'wt', encoding=self.encoding, newline=self.newline)
                 else:
                     logging.warning('docname_as_file=1 but the document contains no docname')
+            elif self.overwrite:
+                docname = document.meta.get('loaded_from', None)
+                if docname is not None:
+                    logging.info('Writing to file %s.', docname)
+                    sys.stdout = open(docname, 'wt', encoding=self.encoding, newline=self.newline)
+                else:
+                    logging.warning('overwrite=1 but documet.meta["loaded_from"] is None')
             else:
                 sys.stdout = self.orig_stdout
         else:
