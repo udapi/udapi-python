3ea67d43143f29f4ec586fc7b0944a763bad1a2f
Martin Popel <popel@ufal.mff.cuni.cz>
2021-03-03 11:44:52 +0100

    tool.morphodita should wrap not only `generate`, but also `analyze`

--- udapi/tool/morphodita.py
+++ udapi/tool/morphodita.py
@@ -1,7 +1,7 @@
 """Wrapper for MorphoDiTa (more pythonic than ufal.morphodita)."""
 from collections import namedtuple
 
-from ufal.morphodita import Morpho, TaggedLemmasForms  # pylint: disable=no-name-in-module
+from ufal.morphodita import Morpho, TaggedLemmasForms, TaggedLemmas  # pylint: disable=no-name-in-module
 from udapi.core.resource import require_file
 
 FormInfo = namedtuple('FormInfo', 'form lemma tag guesser')
@@ -19,8 +19,7 @@ class MorphoDiTa:
             raise IOError("Cannot load model from file '%s'" % path)
 
     def forms_of_lemma(self, lemma, tag_wildcard='?', guesser=True):
-        """Return all forms of a given lemma matching a given tag wildcard."""
-
+        """Return all forms (a list of FormInfo tuples) of a given lemma matching a given tag wildcard."""
         use_guesser = 1 if guesser else 0
         lemmas_forms = TaggedLemmasForms()
         used_guesser = self.tool.generate(lemma, tag_wildcard, use_guesser, lemmas_forms)
@@ -29,3 +28,13 @@ class MorphoDiTa:
             for form in lemma_forms.forms:
                 forms.append(FormInfo(form.form, lemma_forms.lemma, form.tag, used_guesser))
         return forms
+
+    def analyze_form(self, form, guesser=True):
+        """Return all lemma-tag analyses (a list of FormInfo tuples) of a given form."""
+        use_guesser = 1 if guesser else 0
+        tagged_lemmas = TaggedLemmas()
+        used_guesser = self.tool.analyze(form, use_guesser, tagged_lemmas)
+        result = []
+        for tl in tagged_lemmas:
+            result.append(FormInfo(form, tl.lemma, tl.tag, used_guesser))
+        return result
