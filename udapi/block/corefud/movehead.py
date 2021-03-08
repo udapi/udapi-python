abd220953e088b59f08cc2ce4cf97c3c00b2b68f
Martin Popel <popel@ufal.mff.cuni.cz>
2021-03-08 09:56:03 +0100

    better corefud.MoveHead: less warnings, possibility to store all bugs in misc['Bug']

--- udapi/block/corefud/movehead.py
+++ udapi/block/corefud/movehead.py
@@ -6,9 +6,9 @@ from udapi.core.node import find_minimal_common_treelet
 class MoveHead(Block):
     """Block corefud.MoveHead moves the head to the highest node in each mention."""
 
-    def __init__(self, nontreelet='fix', **kwargs):
+    def __init__(self, bugs='warn', **kwargs):
         self.counter = Counter()
-        self.nontreelet = nontreelet
+        self.bugs = bugs
         super().__init__(**kwargs)
 
     def find_head(self, mention):
@@ -19,25 +19,34 @@ class MoveHead(Block):
             self.counter['with_empty'] += 1
             for empty_node in empty_nodes:
                 parents = [d['parent'] for d in empty_node.deps if not d['parent'].is_empty()]
-                if parents and parents[0] not in non_empty:
-                    non_empty.append(parents[0])
+                if parents:
+                    if parents[0] not in non_empty:
+                        non_empty.append(parents[0])
                 else:
                     # TODO we should climb up, but preventing cycles
                     # We could also introduce empty_node.nonempty_ancestor
-                    logging.warning(f"could not find non-empty parent of {empty_node} for mention {mention.head}")
+                    if 'warn' in self.bugs:
+                        logging.warning(f"could not find non-empty parent of {empty_node} for mention {mention.head}")
+                    if 'mark' in self.bugs:
+                        node.misc['Bug'] = 'no-parent-of-empty'
             non_empty.sort()
 
         (highest, added_nodes) = find_minimal_common_treelet(*non_empty)
         if highest in mention.words:
             return highest, 'treelet'
 
-        if 'warn' in self.nontreelet:
+        if 'warn' in self.bugs:
             logging.warning(f"Non-treelet mention in {mention.head} (nearest common antecedent={highest})")
-        if 'mark' in self.nontreelet:
-            node.misc['Mark'] = 'non-treelet-mention'
+        if 'mark' in self.bugs:
+            mention.head.misc['Bug'] = 'non-treelet-mention'
         for word in mention.words:
             if not word.is_empty() and word.parent not in non_empty:
                 return word, 'nontreelet'
+
+        if 'warn' in self.bugs:
+            logging.warning(f"Strange mention {mention.head} (nearest common antecedent={highest})")
+        if 'mark' in self.bugs:
+            mention.head.misc['Bug'] = 'strange-mention'
         return mention.head, 'bug'
 
     def process_document(self, doc):
