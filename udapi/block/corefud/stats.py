c7e16c077618c611b1fcdbdf844d57b60e6a1276
Martin Popel <popel@ufal.mff.cuni.cz>
2021-03-13 02:10:50 +0100

    corefud.Stats now can report also about discountinous/non-treelet mentions and head UPOS distribution

--- udapi/block/corefud/stats.py
+++ udapi/block/corefud/stats.py
@@ -5,12 +5,14 @@ class Stats(Block):
     """Block corefud.Stats prints various coreference-related statistics."""
 
     def __init__(self, m_len_max=5, c_len_max=5, report_mentions=True, report_clusters=True,
+                 report_details=True, selected_upos='NOUN PRON PROPN DET ADJ VERB ADV NUM',
                  exclude_singletons=False, exclude_nonsingletons=False, style='human', **kwargs):
         super().__init__(**kwargs)
         self.m_len_max = m_len_max
         self.c_len_max = c_len_max
         self.report_mentions = report_mentions
         self.report_clusters = report_clusters
+        self.report_details = report_details
         self.exclude_singletons = exclude_singletons
         self.exclude_nonsingletons = exclude_nonsingletons
         self.style = style
@@ -24,7 +26,7 @@ class Stats(Block):
         self.longest_mention = 0
         self.longest_cluster = 0
         self.m_words = 0
-        self.m_empty = 0
+        self.selected_upos = None if selected_upos == 'all' else selected_upos.split()
 
     def process_document(self, doc):
         self.total_nodes += len(list(doc.nodes))
@@ -39,15 +41,30 @@ class Stats(Block):
             self.counter[f"c_len_{min(len_mentions, self.c_len_max)}"] += 1
 
             self.clusters += 1
+            if not self.report_mentions and not self.report_details:
+                continue
             for mention in cluster.mentions:
                 self.mentions += 1
                 all_words = len(mention.words)
                 non_empty = len([w for w in mention.words if not w.is_empty()])
                 self.m_words += all_words
-                self.m_empty += all_words - non_empty
                 self.longest_mention = max(non_empty, self.longest_mention)
                 self.counter['m_total_len'] += non_empty
                 self.counter[f"m_len_{min(non_empty, self.m_len_max)}"] += 1
+                if self.report_details:
+                    upos = 'other'
+                    if not self.selected_upos or mention.head.upos in self.selected_upos:
+                        upos = mention.head.upos
+                    self.counter['m_head_upos_' + upos] += 1
+                    self.counter['m_with_empty'] += 1 if all_words > non_empty else 0
+                    self.counter['m_with_gaps'] += 1 if ',' in mention.span else 0
+                    heads, mwords = 0, set(mention.words)
+                    for w in mention.words:
+                        if w.parent:
+                            heads += 0 if w.parent in mwords else 1
+                        else:
+                            heads += 0 if any(d['parent'] in mwords for d in w.deps) else 1
+                    self.counter['m_nontreelet'] += 1 if heads > 1 else 0
 
     def process_end(self):
         mentions_nonzero = 1 if self.mentions == 0 else self.mentions
@@ -71,6 +88,16 @@ class Stats(Block):
             for i in range(0, self.m_len_max + 1):
                 percent = 100 * self.counter[f"m_len_{i}"] / mentions_nonzero
                 columns.append((f"m_len_{i}{'' if i < self.m_len_max else '+'}", f"{percent:5.1f}"))
+        if self.report_details:
+            columns += [('with_empty', f"{100 * self.counter['m_with_empty'] / mentions_nonzero:5.1f}"),
+                        ('with_gaps', f"{100 * self.counter['m_with_gaps'] / mentions_nonzero:5.1f}"),
+                        ('nontreelet', f"{100 * self.counter['m_nontreelet'] / mentions_nonzero:5.1f}"),]
+            if self.selected_upos:
+                upos_list = self.selected_upos + ['other']
+            else:
+                upos_list = [x[12:] for x in self.counter if x.startswith('m_head_upos_')]
+            for upos in upos_list:
+                columns.append(('head_upos=' + upos, f"{100 * self.counter['m_head_upos_' + upos] / mentions_nonzero:5.1f}"))
 
         if self.style == 'tex':
             print(" & ".join(c[1] for c in columns))
