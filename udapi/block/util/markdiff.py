1eee540ad18c00525941cebc814a76c88550c027
Martin Popel <popel@ufal.mff.cuni.cz>
2021-06-24 17:52:46 +0200

    util.MarkDiff print_stats=6 prints top 6 changes

--- udapi/block/util/markdiff.py
+++ udapi/block/util/markdiff.py
@@ -1,5 +1,7 @@
 """util.MarkDiff is a special block for marking differences between parallel trees."""
+import collections
 import difflib
+import pprint
 from udapi.core.block import Block
 
 
@@ -7,13 +9,25 @@ class MarkDiff(Block):
     """Mark differences between parallel trees."""
 
     def __init__(self, gold_zone, attributes='form,lemma,upos,xpos,deprel,feats,misc',
-                 mark=1, add=False, **kwargs):
-        """Create the Mark block object."""
+                 mark=1, add=False, print_stats=0, **kwargs):
+        """Create the Mark block object.
+        Params:
+        gold_zone: Which of the zones should be treated as gold?
+            (The changes are interpreted as from a "pred"=predicted zone into the gold zone.)
+        attributes: Which node attributes should be considered when searching for diffs?
+            The tree topology, i.e. node parent is always considered.
+        mark: What value should be used in `node.misc['Mark']` of the differing nodes?
+        add: If False, node.misc attributes Mark, ToDo and Bug will be deleted before running this block,
+            so that the marked_only option (e.g. via `udapy -TM`) prints only nodes marked by this block.
+        print_stats: How many lines of statistics should be printed? -1 means all.
+        """
         super().__init__(**kwargs)
         self.gold_zone = gold_zone
         self.attrs = attributes.split(',')
         self.mark = mark
         self.add = add
+        self.print_stats = print_stats
+        self.stats = collections.Counter()
 
     def process_tree(self, tree):
         gold_tree = tree.bundle.get_tree(self.gold_zone)
@@ -49,6 +63,31 @@ class MarkDiff(Block):
                     if alignment.get(p_node.parent.ord - 1) != g_node.parent.ord - 1:
                         p_node.misc['Mark'] = self.mark
                         g_node.misc['Mark'] = self.mark
+                        self.stats['ONLY-PARENT-CHANGED'] += 1
             else:
                 for node in pred_nodes[pred_lo:pred_hi] + gold_nodes[gold_lo:gold_hi]:
                     node.misc['Mark'] = self.mark
+                if self.print_stats:
+                    if edit == 'replace':
+                        # first n nodes are treated as aligned, the rest is treated as ADDED/DELETED
+                        n = min(pred_hi - pred_lo, gold_hi - gold_lo)
+                        for p_node, g_node in zip(pred_nodes[pred_lo:pred_lo + n], gold_nodes[gold_lo:gold_lo + n]):
+                            for attr in self.attrs:
+                                p_value, g_value = p_node._get_attr(attr), g_node._get_attr(attr)
+                                if p_value != g_value:
+                                    self.stats[f'{attr.upper()}: {p_value} -> {g_value}'] += 1
+                            if alignment.get(p_node.parent.ord - 1) != g_node.parent.ord - 1:
+                                self.stats['PARENT-CHANGED'] += 1
+                        pred_lo, gold_lo = pred_lo + n, gold_lo + n
+                    for node in gold_nodes[gold_lo:gold_hi]:
+                        self.stats['ADD-WORD'] += 1
+                        self.stats['ADD-LEMMA: ' + node.lemma] += 1
+                    for node in pred_nodes[pred_lo:pred_hi]:
+                        self.stats['DELETE-WORD'] += 1
+                        self.stats['DELETE-LEMMA: ' + node.lemma] += 1
+
+    def process_end(self):
+        if self.print_stats:
+            how_many = None if self.print_stats in (-1, '-1') else self.print_stats
+            for edit, count in self.stats.most_common(how_many):
+                print(f'{count:4} {edit}')
