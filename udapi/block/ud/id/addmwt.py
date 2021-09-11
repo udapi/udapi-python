70361e213d59709d0793992b01476bf5373a6e46
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-09-11 22:07:29 +0200

    Features of -nya.

--- udapi/block/ud/id/addmwt.py
+++ udapi/block/ud/id/addmwt.py
@@ -11,6 +11,15 @@ class AddMwt(udapi.block.ud.addmwt.AddMwt):
         """Return a dict with MWT info or None if `node` does not represent a multiword token."""
         if node.upos == 'VERB' and re.search(r'nya$', node.form, re.IGNORECASE):
             splitform = re.sub(r'(nya)$', r' \1', re.IGNORECASE)
+            # The verb with -nya typically has Number[psor]=Sing|Person[psor]=3.
+            # Remove these features from the verb and give the pronoun normal features Number=Sing|Person=3.
+            if node.feats["Number[psor]"] != "Sing":
+                logging.warning("Verb '%s' has Number[psor]=='%s'" % (node.form, node.feats["Number[psor]"]))
+            if node.feats["Person[psor]"] != "3":
+                logging.warning("Verb '%s' has Person[psor]=='%s'" % (node.form, node.feats["Person[psor]"]))
+            node.feats["Number[psor]"] = ''
+            node.feats["Person[psor]"] = ''
+            pronfeats = 'Number=Sing|Person=3|PronType=Prs'
             # 'main': 0 ... this is the default value (the first node will be the head and inherit children)
-            return {'form': splitform, 'lemma': splitform, 'upos': 'VERB PRON', 'shape': 'subtree', 'deprel': '* obj'}
+            return {'form': splitform, 'lemma': splitform, 'upos': 'VERB PRON', 'feats': '* '+pronfeats, 'shape': 'subtree', 'deprel': '* obj'}
         return None
