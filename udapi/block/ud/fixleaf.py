49f1f4385e4006895708f4e6f22ea06c3bc3723f
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-10-30 23:17:19 +0200

    Fix leaf-aux-cop.

---
+++ udapi/block/ud/fixleaf.py
@@ -0,0 +1,35 @@
+"""
+Block ud.FixLeaf checks that function word dependents are leaves.
+Certain known exceptions are observed (e.g., fixed expressions).
+"""
+from udapi.core.block import Block
+import logging
+import re
+
+class FixLeaf(Block):
+    """
+    Make sure that aux and cop dependents are leaves unless one of the known
+    exceptions applies.
+    """
+
+    def __init__(self, deprels='aux,cop', **kwargs):
+        """
+        Args:
+        deprels: comma-separated list of deprels to be fixed. Default = aux,cop.
+        """
+        super().__init__(**kwargs)
+        self.deprels = deprels.split(',')
+
+    def process_node(self, node):
+        for deprel in self.deprels:
+            if node.udeprel == deprel:
+                children = node.children
+                # Every function dependent can have a fixed child.
+                # We will also allow conj, cc, punct, goeswith, reparandum.
+                children = [c for c in children if not re.match(r'^(fixed|conj|cc|punct|goeswith|reparandum)$', c.udeprel)]
+                # Re-attach the remaining children to an acceptable ancestor.
+                ancestor = node.parent
+                while ancestor.udeprel in self.deprels:
+                    ancestor = ancestor.parent
+                for c in children:
+                    c.parent = ancestor
