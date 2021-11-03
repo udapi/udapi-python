9528d7cf5d4927c64fba305a0ced8b32449fec4a
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-11-03 14:41:54 +0100

    FixLeaf cc.

--- udapi/block/ud/fixleaf.py
+++ udapi/block/ud/fixleaf.py
@@ -12,10 +12,10 @@ class FixLeaf(Block):
     exceptions applies.
     """
 
-    def __init__(self, deprels='aux,cop', **kwargs):
+    def __init__(self, deprels='aux,cop,cc', **kwargs):
         """
         Args:
-        deprels: comma-separated list of deprels to be fixed. Default = aux,cop.
+        deprels: comma-separated list of deprels to be fixed. Default = aux,cop,case,mark,cc.
         """
         super().__init__(**kwargs)
         self.deprels = deprels.split(',')
@@ -23,10 +23,12 @@ class FixLeaf(Block):
     def process_node(self, node):
         for deprel in self.deprels:
             if node.udeprel == deprel:
-                children = node.children
                 # Every function dependent can have a fixed child.
                 # We will also allow conj, cc, punct, goeswith, reparandum.
-                children = [c for c in children if not re.match(r'^(fixed|conj|cc|punct|goeswith|reparandum)$', c.udeprel)]
+                allowed = ['fixed', 'punct', 'goeswith', 'reparandum']
+                if deprel != 'cc':
+                    allowed += ['conj', 'cc']
+                children = [c for c in node.children if not (c.udeprel in allowed)]
                 # Re-attach the remaining children to an acceptable ancestor.
                 ancestor = node.parent
                 while ancestor.udeprel in self.deprels:
