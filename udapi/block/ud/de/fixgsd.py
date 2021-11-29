fa25c0ed4a25026270c5a7f96287804c7a1e89cd
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-11-29 11:44:36 +0100

    More fixes of ordinals in German.

--- udapi/block/ud/de/fixgsd.py
+++ udapi/block/ud/de/fixgsd.py
@@ -17,7 +17,7 @@ class FixGSD(Block):
         # to an ordinal numeral at the same time.
         if node.form == '.' and node.next_node:
             # Ignore number+period combinations that have an intervening space.
-            if node.prev_node and re.match('^\d+$', node.prev_node.form) and node.prev_node.no_space_after:
+            if node.prev_node and re.match(r'^\d+$', node.prev_node.form) and node.prev_node.no_space_after:
                 # Merge the number and the period into one token.
                 number = node.prev_node
                 period = node
@@ -35,3 +35,24 @@ class FixGSD(Block):
                 if number.udeprel == 'nummod':
                     number.deprel = 'amod'
                 period.remove()
+        # Even if the digits and the period are already in one token, check their annotation.
+        if re.match(r'^\d+\.$', node.form):
+            node.lemma = node.form
+            node.upos = 'ADJ'
+            node.xpos = 'ADJA'
+            node.feats = '_'
+            node.feats['NumType'] = 'Ord'
+            if node.udeprel == 'nummod':
+                node.deprel = 'amod'
+        # Finally, make sure that ordinal numerals expressed verbosely are tagged properly.
+        # Unlike for digits, do not remove the features for Gender, Number, and Case.
+        # Skip 'acht' because we cannot reliably distinguish it from the cardinal numeral and from the verb 'achten'.
+        if re.match(r'^(erst|zweit|dritt|viert|fünft|sechst|siebt|neunt|(drei|vier|fünf|sechs|sieb|acht|neun)?zehnt|elft|zwölft)(er)?$', node.lemma, re.IGNORECASE):
+            # Skip 'erst' that is used as an adverb.
+            if node.lemma != 'erst' or node.upos != 'ADV':
+                node.lemma = re.sub(r'^(.+)er$', r'\1', node.lemma)
+                node.upos = 'ADJ'
+                node.xpos = 'ADJA'
+                node.feats['NumType'] = 'Ord'
+                if node.udeprel == 'nummod':
+                    node.deprel = 'amod'
