1b437618b1a5a40de1da261d8ed56df6a0097d86
Michal Novak <mnovak@ufal.mff.cuni.cz>
2021-12-07 13:53:41 +0100

    forgotten commit: no debug prints, _set_root_deprel must be called from process_document, otherwise it's not called at all

--- udapi/block/corefud/fixtovalidate.py
+++ udapi/block/corefud/fixtovalidate.py
@@ -3,10 +3,10 @@ from udapi.core.block import Block
 class FixToValidate(Block):
     """This block fixes the CorefUD data so that the final documents are valid conllu files."""
 
-    def _set_root_deprel(self, node):
-        if node.parent == node.root and node.deprel != "root":
-            node.deprel = "root"
-            print(node)
+    def _set_root_deprel(self, doc):
+        for node in doc.nodes:
+            if node.parent == node.root and node.deprel != "root":
+                node.deprel = "root"
 
     def _space_before_pardoc(self, doc):
         last_node = None
@@ -14,11 +14,8 @@ class FixToValidate(Block):
             if i > 0:
                 if (tree.newdoc is not None or tree.newpar is not None) and last_node.no_space_after:
                     del last_node.misc["SpaceAfter"]
-                    print(tree)
             last_node = tree.descendants[-1]
 
-    def process_node(self, node):
-        self._set_root_deprel(node)
-
     def process_document(self, doc):
+        self._set_root_deprel(doc)
         self._space_before_pardoc(doc)
