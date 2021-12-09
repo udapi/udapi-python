c404eb98e7276fce10fa1c0d13569e34e69df5d4
Michal Novak <mnovak@ufal.mff.cuni.cz>
2021-12-09 18:35:27 +0100

    fixing the root-is-not-0 UD validation errors for some of the automatically parsed datasets

--- udapi/block/corefud/fixtovalidate.py
+++ udapi/block/corefud/fixtovalidate.py
@@ -9,6 +9,22 @@ class FixToValidate(Block):
                 if node.deprel != "root":
                     node.deprel = "root"
 
+    def _unset_root_deprel(self, doc):
+        for node in doc.nodes:
+            parent = node.parent
+            if node.deprel == "root" and parent is not None and not parent.is_root():
+                #print("\t".join(['Non-0-root:', node.address(), node.upos, str(node.feats), node.parent.upos, str(node.parent.feats)]))
+                if parent.upos == "PUNCT" and parent.parent is not None:
+                    node.parent = parent.parent
+                if node.upos == "CCONJ":
+                    node.deprel = "cc"
+                elif node.upos == "ADJ" and parent.upos == "PROPN":
+                    node.deprel = "amod"
+                elif node.upos == "NOUN" and parent.upos == "VERB":
+                    node.deprel = "obl"
+                else:
+                    node.deprel = "parataxis"
+
     def _space_before_pardoc(self, doc):
         last_node = None
         for i, tree in enumerate(doc.trees):
@@ -19,4 +35,5 @@ class FixToValidate(Block):
 
     def process_document(self, doc):
         self._set_root_deprel(doc)
+        self._unset_root_deprel(doc)
         self._space_before_pardoc(doc)
