0d409b6cf86a3cf5881a9dfb37e0f39fa3409cb5
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-12-19 23:42:21 +0100

    Bug fix.

--- udapi/block/ud/cs/fixedeprels.py
+++ udapi/block/ud/cs/fixedeprels.py
@@ -12,7 +12,7 @@ class FixEdeprels(Block):
         abbreviation and its morphological case is unknown.
         """
         for edep in node.deps:
-            if edep['deprel'] eq 'nmod:na':
+            if edep['deprel'] == 'nmod:na':
                 # The case is unknown. We need 'acc' or 'loc'.
                 # The locative is probably more frequent but it is not so likely with every noun.
                 if re.match(r'^(adresát|AIDS|DEM|frank|h|ha|hodina|Honolulu|jméno|koruna|litr|metr|míle|miliarda|milión|mm|MUDr|NATO|obyvatel|OSN|počet|procento|příklad|rok|SSSR|vůz)$', node.lemma):
