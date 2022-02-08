c30fd505d4cd3a5b8b54f9207ee22df488f56c32
Martin Popel <popel@ufal.mff.cuni.cz>
2022-02-08 12:19:23 +0100

    rename bridging relations as suggested by @dan-zeman
    in https://github.com/ufal/corefUD/issues/29#issuecomment-1032004247

--- udapi/block/corefud/fixcorefud02.py
+++ udapi/block/corefud/fixcorefud02.py
@@ -2,6 +2,7 @@ from udapi.core.block import Block
 import udapi.core.coref
 import itertools
 
+
 class FixCorefUD02(Block):
     """Fix errors in CorefUD 0.2 for release of CorefUD 1.0."""
 
@@ -11,4 +12,9 @@ class FixCorefUD02(Block):
             #    cluster.cluster_type = cluster.cluster_type.lower()
             for mention in cluster.mentions:
                 for bridge in mention.bridging:
-                    bridge.relation = bridge.relation.lower().replace('-', '')
+                    rel = bridge.relation.lower()
+                    if rel.endswith('-inv'):
+                        rel = 'i' + rel.replace('-inv', '')
+                    rel = rel.replace('-', '')
+                    rel = rel.replace('indirect_', '')
+                    bridge.relation = rel
