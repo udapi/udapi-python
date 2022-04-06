5a8b7513e14e1d1c56f977e93a7985f31538e8c0
Martin Popel <popel@ufal.mff.cuni.cz>
2022-04-06 02:03:31 +0200

    call only the block methods which are needed (i.e. which are overriden)
    There is no need to iterate over all bundles/trees/nodes if a given block
    overrides only process_coref_entity().
    Also, the detection of "No processing activity defined in block"
    had to be adapted to the new methods process_coref_entity and process_coref_mention.
    
    TODO: benchmark if calling process_node directly from process_document
    is actually faster than calling it via process_bundle and process_tree.

--- udapi/core/block.py
+++ udapi/core/block.py
@@ -1,7 +1,7 @@
 """Block class represents the basic Udapi processing unit."""
 import logging
 
-def not_overriden(method):
+def not_overridden(method):
   method.is_not_overridden = True
   return method
 
@@ -26,10 +26,12 @@ class Block(object):
         """A hook method that is executed after processing all UD data"""
         pass
 
+    @not_overridden
     def process_node(self, _):
         """Process a UD node"""
-        raise Exception("No processing activity defined in block " + str(self))
+        pass
 
+    @not_overridden
     def process_tree(self, tree):
         """Process a UD tree"""
         # tree.descendants is slightly slower than tree._descendants (0.05s per iterating over 700k words),
@@ -39,6 +41,7 @@ class Block(object):
         for node in tree.descendants:
             self.process_node(node)
 
+    @not_overridden
     def process_bundle(self, bundle):
         """Process a UD bundle"""
         for tree in bundle:
@@ -57,11 +60,6 @@ class Block(object):
 
     def process_document(self, document):
         """Process a UD document"""
-        for bundle_no, bundle in enumerate(document.bundles, 1):
-            logging.debug('Block %s processing bundle #%d (id=%s)',
-                          self.__class__.__name__, bundle_no, bundle.bundle_id)
-            self.process_bundle(bundle)
-
         # Calling document.coref_entities is expensive because
         # it needs to deserialize coref_entities from the MISC attributes.
         # If no block in a scenario needs to process coreference entities/mentions,
@@ -69,21 +67,44 @@ class Block(object):
         # So we need to detect if any of the methods process_coref_entity and process_coref_mention
         # has been overriden (without calling them, which could have adverse side effects).
         # Let's use method annotations for this.
-        proc_entities = not hasattr(self.process_coref_entity, 'is_not_overriden')
-        proc_mentions = not hasattr(self.process_coref_mention, 'is_not_overriden')
-        if proc_entities or proc_mentions:
+        p_entity = not hasattr(self.process_coref_entity, 'is_not_overridden')
+        p_mention = not hasattr(self.process_coref_mention, 'is_not_overridden')
+        p_bundle = not hasattr(self.process_bundle, 'is_not_overridden')
+        p_tree = not hasattr(self.process_tree, 'is_not_overridden')
+        p_node = not hasattr(self.process_node, 'is_not_overridden')
+        if not any((p_entity, p_mention, p_bundle, p_tree, p_node)):
+            raise Exception("No processing activity defined in block " + str(self))
+
+        if p_entity or p_mention:
             for entity in document.coref_entities:
-                self.process_coref_entity(entity)
-                if proc_mentions:
+                if p_entity:
+                    self.process_coref_entity(entity)
+                else:
                     for mention in entity.mentions:
                         self.process_coref_mention(mention)
 
-    @not_overriden
+        if p_bundle or p_tree or p_node:
+            for bundle_no, bundle in enumerate(document.bundles, 1):
+                logging.debug('Block %s processing bundle #%d (id=%s)',
+                            self.__class__.__name__, bundle_no, bundle.bundle_id)
+                if p_bundle:
+                    self.process_bundle(bundle)
+                else:
+                    for tree in bundle:
+                        if self._should_process_tree(tree):
+                            if p_tree:
+                                self.process_tree(tree)
+                            else:
+                                for node in tree.descendants:
+                                    self.process_node(node)
+
+    @not_overridden
     def process_coref_entity(self, entity):
         """This method is called on each coreference entity in the document."""
-        pass
+        for mention in entity.mentions:
+            self.process_coref_mention(mention)
 
-    @not_overriden
+    @not_overridden
     def process_coref_mention(self, mention):
         """This method is called on each coreference mention in the document."""
         pass
