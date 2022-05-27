9d8dc3b0568596a1d8ff16dc9f54504ea38a583a
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-05-27 13:08:32 +0200

    Fix spurious auxiliaries in Kazakh.

---
+++ udapi/block/ud/kk/fixspuriousaux.py
@@ -0,0 +1,24 @@
+"""Block to convert spurious auxiliaries to lexical verbs in Kazakh."""
+from udapi.core.block import Block
+import logging
+import re
+
+class FixSpuriousAux(Block):
+
+    def process_node(self, node):
+        """
+        Some verbs that are called auxiliary by the traditional grammar, should
+        be analyzed in UD as VERB + non-finite xcomp.
+        """
+        if node.upos == 'AUX' and node.udeprel == 'aux':
+            # баста = start
+            if re.match(r'^(баста)$', node.lemma):
+                node.upos = 'VERB'
+                # The auxiliary inherits the incoming relation of its original parent.
+                lexverb = node.parent
+                node.parent = lexverb.parent
+                node.deprel = lexverb.deprel
+                # The auxiliary also inherits some but not all children of the lexical verb.
+                for c in lexverb.children:
+                    if re.match(r'^(nsubj|csubj|obl|advmod|advcl|vocative|discourse|parataxis|punct)$', c.udeprel):
+                        c.parent = node
