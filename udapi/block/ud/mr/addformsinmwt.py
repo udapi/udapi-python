29c48fa5a9002e37d14fe610d942586a06c9d328
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-05-28 16:51:48 +0200

    Restore forms of words within multiword tokens.

---
+++ udapi/block/ud/mr/addformsinmwt.py
@@ -0,0 +1,27 @@
+"""
+Block ud.mr.AddFormsInMwt looks for multiword tokens whose words lack forms.
+Based on the form of the surface token and on the information provided in
+the lemmas and UPOS, tries to reconstruct the forms of individual words.
+"""
+from udapi.core.block import Block
+import re
+import logging
+
+
+class AddFormsInMwt(Block):
+    """Guess forms of syntactic worms within a multiword token."""
+
+    def process_node(self, node):
+        if node.form == '_' and node.multiword_token:
+            mwt = node.multiword_token
+            # Many multiword tokens consist of NOUN + ADP. Beware: The adposition
+            # may have a form different from its lemma. It happens with possessive
+            # postpositions चा, चे, which distinguish the gender and number of
+            # the possessed entity.
+            if len(mwt.words) == 2 and mwt.words[1].upos == 'ADP':
+                if mwt.form == mwt.words[0].lemma + mwt.words[1].lemma:
+                    node.form = node.lemma
+                else:
+                    logging.info("Cannot decompose %s+ADP multiword token '%s'. Part lemmas are '%s' and '%s'." % (mwt.words[0].upos, mwt.form, mwt.words[0].lemma, mwt.words[1].lemma))
+            else:
+                logging.info("Cannot decompose multiword token '%s' of %d parts: %s" % (mwt.form, len(mwt.words), str([x.lemma for x in mwt.words])))
