d63c6f8cb06d6e42a3742a0b8c518a39d5da1f32
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-04-18 10:07:22 +0200

    Extended the ElQue block. Now it can also harmonize the annotation.

--- udapi/block/ud/ca/elque.py
+++ udapi/block/ud/ca/elque.py
@@ -9,6 +9,14 @@ import re
 
 class ElQue(Block):
 
+    def __init__(self, fix=False, **kwargs):
+        """
+        Default: Print the annotation patterns but do not fix anything.
+        fix=1: Do not print the patterns but fix them.
+        """
+        super().__init__(**kwargs)
+        self.fix = fix
+
     def process_node(self, node):
         # We take 'que' as the central node of the construction.
         if node.lemma == 'que' and node.upos == 'PRON' and node.parent.ord > node.ord:
@@ -19,35 +27,53 @@ class ElQue(Block):
             # Check the lemma of the determiner. The form may vary for gender and number.
             if que.prev_node and que.prev_node.lemma == 'el':
                 el = que.prev_node
-                stanford = []
                 adp = None
                 if el.prev_node and el.prev_node.upos == 'ADP':
                     adp = el.prev_node
                     if adp.udeprel == 'fixed':
                         adp = adp.parent
-                    parentstr = 'OTHER'
-                    if adp.parent == el:
-                        parentstr = 'el'
-                    elif adp.parent == que:
-                        parentstr = 'que'
-                    elif adp.parent == verb:
-                        parentstr = 'VERB'
-                    stanford.append(adp.deprel + '(' + parentstr + ', ADP)')
-                if el.parent == adp:
-                    parentstr = 'ADP'
-                elif el.parent == que:
-                    parentstr = 'que'
-                elif el.parent == verb:
-                    parentstr = 'VERB'
+                if self.fix:
+                    self.fix_pattern(adp, el, que, verb)
                 else:
-                    parentstr = 'OTHER'
-                stanford.append(el.deprel + '(' + parentstr + ', el)')
-                stanford.append(que.deprel + '(VERB, que)')
-                if verb.parent == adp:
-                    parentstr = 'ADP'
-                elif verb.parent == el:
-                    parentstr = 'el'
-                else:
-                    parentstr = 'OTHER'
-                stanford.append(verb.deprel + '(' + parentstr + ', VERB)')
-                print('; '.join(stanford))
+                    self.print_pattern(adp, el, que, verb)
+
+    def print_pattern(self, adp, el, que, verb):
+        stanford = []
+        if adp:
+            if adp.parent == el:
+                parentstr = 'el'
+            elif adp.parent == que:
+                parentstr = 'que'
+            elif adp.parent == verb:
+                parentstr = 'VERB'
+            else:
+                parentstr = 'OTHER'
+            stanford.append(adp.deprel + '(' + parentstr + ', ADP)')
+        if el.parent == adp:
+            parentstr = 'ADP'
+        elif el.parent == que:
+            parentstr = 'que'
+        elif el.parent == verb:
+            parentstr = 'VERB'
+        else:
+            parentstr = 'OTHER'
+        stanford.append(el.deprel + '(' + parentstr + ', el)')
+        # We found the verb as the parent of 'que', so we do not need to check the parent of 'que' now.
+        stanford.append(que.deprel + '(VERB, que)')
+        if verb.parent == adp:
+            parentstr = 'ADP'
+        elif verb.parent == el:
+            parentstr = 'el'
+        else:
+            parentstr = 'OTHER'
+        stanford.append(verb.deprel + '(' + parentstr + ', VERB)')
+        print('; '.join(stanford))
+
+    def fix_pattern(self, adp, el, que, verb):
+        if adp:
+            if adp.parent == que or adp.parent == verb:
+                adp.parent = el
+                adp.deprel = 'case'
+                if len(adp.deps) == 1:
+                    adp.deps[0]['parent'] = el
+                    adp.deps[0]['deprel'] = 'case'
