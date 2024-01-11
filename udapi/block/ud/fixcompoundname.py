02698410b6c7c5a17b092afbbc0044ab1a0b5515
Dan Zeman <zeman@ufal.mff.cuni.cz>
2024-01-11 18:11:33 +0100

    Improved block ud.FixCompoundName.

--- udapi/block/ud/fixcompoundname.py
+++ udapi/block/ud/fixcompoundname.py
@@ -7,6 +7,7 @@ should be analyzed as flat but there are treebanks that incorrectly use compound
 for person names. This block can be used to fix them.
 """
 from udapi.core.block import Block
+import regex as re
 import logging
 
 
@@ -19,18 +20,26 @@ class FixCompoundName(Block):
 
     def process_node(self, node):
         if node.upos == 'PROPN' and node.udeprel == 'compound' and node.parent.upos == 'PROPN':
+            origparent = node.parent
+            grandparent = origparent.parent
+            outdeprel = origparent.deprel
             # See if there are other PROPN compound siblings.
-            namewords = [x for x in node.siblings if x.upos == 'PROPN' and x.udeprel == 'compound']
-            namewords.append(node.parent)
-            namewords = sorted(namewords, key=lambda x: x.ord)
-            ###!!! We currently cannot transform enhanced dependencies.
-            ###!!! If we proceed, the basic tree would diverge from the enhanced dependencies.
-            if len(node.deps) > 0:
-                logging.fatal('There are enhanced dependencies but ud.FixCompoundName has been implemented only for basic dependencies.')
-            # The first name word will be the technical head. If it is the current parent, fine.
-            if namewords[0] != node.parent:
-                namewords[0].parent = node.parent.parent
-                namewords[0].deprel = node.parent.deprel
-            for i in range(len(namewords)-1):
-                namewords[i+1].parent = namewords[0]
-                namewords[i+1].deprel = 'flat:name'
+            namewords = sorted([x for x in origparent.children(add_self=True) if x.upos == 'PROPN' and (x.udeprel == 'compound' or x == origparent)], key=lambda y: y.ord)
+            # The Hindi treebank tags dates (['30', 'navaṁbara'], ['disaṁbara', '1993']) as PROPN compounds.
+            # This is wrong but it is also different from personal names we are targeting here.
+            # Hence, we will skip "names" that contain numbers.
+            if len([x for x in namewords if re.search(r"\d", x.form)]) == 0:
+                #logging.info(str([x.misc['Translit'] for x in namewords]))
+                ###!!! We currently cannot transform enhanced dependencies.
+                ###!!! If we proceed, the basic tree would diverge from the enhanced dependencies.
+                if len(node.deps) > 0:
+                    logging.fatal('There are enhanced dependencies but ud.FixCompoundName has been implemented only for basic dependencies.')
+                # The first name word will be the technical head. If it is the current parent, fine.
+                head = namewords[0]
+                rest = namewords[1:]
+                if head != origparent:
+                    head.parent = grandparent
+                    head.deprel = outdeprel
+                for n in rest:
+                    n.parent = head
+                    n.deprel = 'flat:name'
