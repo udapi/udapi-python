a76d15bad7406fe1a79d2f9d1d95ae7a5324dd82
Michal Novak <mnovak@ufal.mff.cuni.cz>
2024-04-13 15:43:37 +0200

    deps to empty tokens must be recursively redirected to the non-empty ancestors

--- udapi/block/corefud/delete.py
+++ udapi/block/corefud/delete.py
@@ -10,19 +10,43 @@ class Delete(Block):
         super().__init__(**kwargs)
         self.empty = empty
 
+
+    def _deps_ignore_nodes(self, node, parents_to_ignore):
+        """ Retrieve deps from the node, recursively ignoring specified parents.
+        """
+        newdeps = []
+        stack = [(node, [])]
+        while stack:
+            proc_node, skipped_nodes = stack.pop()
+            # if there is a cycle of skipped nodes, ground the subtree to the root
+            if proc_node in skipped_nodes:
+                newdeps.append({'parent': node.root, 'deprel': 'root'})
+                continue
+            for dep in proc_node.deps:
+                # keep deps with a parent that shouldn't be ignored
+                if not dep['parent'] in parents_to_ignore:
+                    newdeps.append(dep)
+                    continue
+                # process the ignored parent recursively
+                stack.append((dep['parent'], skipped_nodes + [proc_node]))
+        return newdeps
+
     def process_document(self, doc):
         # This block should work both with coreference loaded (deserialized) and not.
         doc._eid_to_entity = None
         for root in doc.trees:
             if self.empty:
-                root.empty_nodes = []
                 for node in root.descendants:
-                    if node.raw_deps != '_':
-                        node.raw_deps = '|'.join(d for d in node.raw_deps.split('|') if not '.' in d)
-                        if node.raw_deps == '':
-                            node.raw_deps = '0:root'
+                    # process only the nodes dependent on empty nodes
+                    if not '.' in node.raw_deps:
+                        continue
+                    newdeps = self._deps_ignore_nodes(node, root.empty_nodes)
+                    newdeps_sorted = sorted(set((dep['parent'].ord, dep['deprel']) for dep in newdeps))
+                    node.raw_deps = '|'.join(f"{p}:{r}" for p, r in newdeps_sorted)
+
                     if '.' in node.misc['Functor'].split(':')[0]:
                         del node.misc['Functor']
+                root.empty_nodes = []
 
             for node in root.descendants + root.empty_nodes:
                 node._mentions = []
