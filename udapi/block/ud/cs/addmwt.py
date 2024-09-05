7aa646ca5d1899da81fc9bfb3b0165b7a7da618f
Dan Zeman <zeman@ufal.mff.cuni.cz>
2024-09-05 23:27:49 +0200

    Updated rules for multiword tokens in Czech.

--- udapi/block/ud/cs/addmwt.py
+++ udapi/block/ud/cs/addmwt.py
@@ -1,13 +1,14 @@
 """Block ud.cs.AddMwt for heuristic detection of multi-word tokens."""
 import udapi.block.ud.addmwt
 
+# Define static rules for 'aby', 'kdyby' and similar forms.
 MWTS = {
     'abych': {'form': 'aby bych', 'feats': '_ Mood=Cnd|Number=Sing|Person=1|VerbForm=Fin'},
     'kdybych': {'form': 'když bych', 'feats': '_ Mood=Cnd|Number=Sing|Person=1|VerbForm=Fin'},
     'abys': {'form': 'aby bys', 'feats': '_ Mood=Cnd|Number=Sing|Person=2|VerbForm=Fin'},
     'kdybys': {'form': 'když bys', 'feats': '_ Mood=Cnd|Number=Sing|Person=2|VerbForm=Fin'},
-    'aby': {'form': 'aby by', 'feats': '_ Mood=Cnd|Person=3|VerbForm=Fin'},
-    'kdyby': {'form': 'když by', 'feats': '_ Mood=Cnd|Person=3|VerbForm=Fin'},
+    'aby': {'form': 'aby by', 'feats': '_ Mood=Cnd|VerbForm=Fin'},
+    'kdyby': {'form': 'když by', 'feats': '_ Mood=Cnd|VerbForm=Fin'},
     'abychom': {'form': 'aby bychom', 'feats': '_ Mood=Cnd|Number=Plur|Person=1|VerbForm=Fin'},
     'kdybychom': {'form': 'když bychom', 'feats': '_ Mood=Cnd|Number=Plur|Person=1|VerbForm=Fin'},
     'abyste': {'form': 'aby byste', 'feats': '_ Mood=Cnd|Number=Plur|Person=2|VerbForm=Fin'},
@@ -25,13 +26,14 @@ for v in MWTS.values():
         person = '1'
     elif 'Person=2' in v['feats']:
         person = '2'
-
     v['xpos'] = 'J,------------- Vc-%s---%s-------' % (number, person)
     v['deprel'] = '* aux'
     v['lemma'] = v['form'].split()[0] + ' být'
     v['main'] = 0
     v['shape'] = 'siblings'
 
+# Define static rules for 'nač', 'zač', 'oč' (but not 'proč').
+# Add them to the already existing dictionary MWTS.
 # nač -> na + co
 for prep in 'na za o'.split():
     MWTS[prep + 'č'] = {
@@ -43,7 +45,6 @@ for prep in 'na za o'.split():
         'shape': 'subtree',
     }
 
-
 class AddMwt(udapi.block.ud.addmwt.AddMwt):
     """Detect and mark MWTs (split them into words and add the words to the tree)."""
 
@@ -52,21 +53,20 @@ class AddMwt(udapi.block.ud.addmwt.AddMwt):
         analysis = MWTS.get(node.form.lower(), None)
         if analysis is not None:
             return analysis
-
-        # There is no VerbType=verbconj in the UD_Czech data.
-        # The purpose of this rule is rather to show that
-        # it is possible to write such "dynamic" rules
-        # (which cannot be included in static MWTS).
-        if node.form.lower().endswith('ť') and node.feats['VerbType'] == 'verbconj':
+        # If the node did not match any of the static rules defined in MWTS,
+        # check it against the "dynamic" rule below. The enclitic 'ť' will be
+        # separated from its host but only if it has been marked by an annotator
+        # in MISC.
+        if node.form.lower().endswith('ť') and node.misc['RETOKENIZE'] == 'rozdělit':
             return {
-                'form': node.form.lower()[:-1] + ' neboť',
-                'lemma': '* neboť',
-                'upos': '* CCONJ',
-                'xpos': 'Vt-S---3P-NA--2 J^-------------',
-                'feats': '* _',
-                'deprel': '* cc',
-                'main': 0,
-                'shape': 'subtree',
+                'form':   node.form.lower()[:-1] + ' ť',
+                'lemma':  '* ť',
+                'upos':   '* PART',
+                'xpos':   '* TT-------------',
+                'feats':  '* _',
+                'deprel': '* discourse',
+                'main':   0,
+                'shape':  'subtree',
             }
         return None
 
