b4bc8d7e14b253b06ab54805bede40670e16bc91
Dan Zeman <zeman@ufal.mff.cuni.cz>
2024-12-20 21:12:34 +0100

    Guess case of nominals that lack it.

--- udapi/block/mwe/msfinit.py
+++ udapi/block/mwe/msfinit.py
@@ -13,3 +13,26 @@ class MsfInit(Block):
         """
         for f in node.feats:
             node.misc['MSF'+f] = node.feats[f]
+        # We are particularly interested in the Case feature but some nominals
+        # lack it (e.g. acronyms or numbers). If there is a preposition, it may
+        # indicate the expected case of the nominal.
+        if not node.feats['Case']:
+            # Not any 'case' dependent is helpful. Here we really need single-word
+            # adposition.
+            adpositions = [x for x in node.children if x.udeprel == 'case' and x.upos == 'ADP']
+            if len(adpositions) == 1:
+                fixed = [x for x in adpositions[0].children if x.udeprel == 'fixed']
+                if not fixed and adpositions[0].feats['Case']:
+                    node.misc['MSFCase'] = adpositions[0].feats['Case']
+        # If we did not find a preposition to help us, we may be able to read
+        # the case off an adjectival modifier or determiner.
+        if not node.misc['MSFCase']:
+            modifiers = [x for x in node.children if x.udeprel in ['amod', 'det'] and x.feats['Case']]
+            if modifiers:
+                node.misc['MSFCase'] = modifiers[0].feats['Case']
+        # Finally, if the above did not help, we may guess the case from the deprel of the node itself.
+        if not node.misc['MSFCase']:
+            if node.udeprel == 'nsubj':
+                node.misc['MSFCase'] = 'Nom'
+            elif node.udeprel == 'obj':
+                node.misc['MSFCase'] = 'Acc'
