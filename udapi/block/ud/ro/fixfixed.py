12c96924eb5168897bdd452ef3eb85ebc41cfcc8
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-07-01 16:08:56 +0200

    New fixed expr

--- udapi/block/ud/ro/fixfixed.py
+++ udapi/block/ud/ro/fixfixed.py
@@ -11,14 +11,9 @@ class FixFixed(Block):
     """Block for fixing verbal 'fixed' expressions."""
 
     def process_node(self, node):
-        if node.upos=='VERB':
-            fixchildren = [x for x in node.children if x.udeprel=='fixed']
-            nfc = len(fixchildren)
-            if nfc==1 and fixchildren[0].upos == 'NOUN':
-                fixchildren[0].deprel = 'obj'
-            elif nfc==2 and fixchildren[1].upos == 'NOUN':
-                fixchildren[0].parent = fixchildren[1]
-                fixchildren[0].deprel = 'case'
-                fixchildren[1].deprel = 'obl'
-            elif nfc>0:
-                logging.info('Another case: '+node.lemma+' '+' '.join([x.form for x in fixchildren]))
+        fixchildren = [x for x in node.children if x.udeprel=='fixed']
+        nfc = len(fixchildren)
+        if nfc>0 and node.udeprel=="advmod":
+            node.feats['ExtPos'] = 'ADV'
+        #elif nfc>0:
+        #    logging.info('Another case: '+node.lemma+' '+' '.join([x.form for x in fixchildren]))
