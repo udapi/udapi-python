5b19027f33d17286377b2eb381e33cbcd44b5e5b
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-07-01 16:26:12 +0200

    Romanian fixed expressions.

--- udapi/block/ud/ro/fixfixed.py
+++ udapi/block/ud/ro/fixfixed.py
@@ -8,12 +8,13 @@ from udapi.core.block import Block
 
 
 class FixFixed(Block):
-    """Block for fixing verbal 'fixed' expressions."""
+    """Block for fixing annotation of some 'fixed' expressions."""
 
     def process_node(self, node):
         fixchildren = [x for x in node.children if x.udeprel=='fixed']
         nfc = len(fixchildren)
-        if nfc>0 and node.udeprel=="advmod":
-            node.feats['ExtPos'] = 'ADV'
-        #elif nfc>0:
-        #    logging.info('Another case: '+node.lemma+' '+' '.join([x.form for x in fixchildren]))
+        if nfc > 0:
+            if node.udeprel == 'advmod' and node.feats['ExtPos'] == '':
+                node.feats['ExtPos'] = 'ADV'
+            elif node.feats['ExtPos'] == '':
+                logging.info('Another case: '+node.lemma+' '+' '.join([x.form for x in fixchildren]))
