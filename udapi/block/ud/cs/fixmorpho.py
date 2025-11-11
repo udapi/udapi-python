cd4b706cdfd752221c23e09be177891fe3620a02
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-11-11 16:51:57 +0100

    Fix reflexive pronouns (particles) in Czech.

--- udapi/block/ud/cs/fixmorpho.py
+++ udapi/block/ud/cs/fixmorpho.py
@@ -6,12 +6,38 @@ of words (e.g. whether or not nouns should have the Polarity feature). It was
 created as part of the Hičkok project (while importing nineteenth-century Czech
 data) but it should be applicable on any other Czech treebank.
 """
-import udapi.block
+from udapi.core.block import Block
+import logging
 import re
 
-class FixMorpho(udapi.block):
+class FixMorpho(Block):
 
     def process_node(self, node):
+        # Do not touch words marked as Foreign or Typo. They may not behave the
+        # way we expect in Czech data.
+        if node.feats['Foreign'] == 'Yes' or node.feats['Typo'] == 'Yes':
+            return
+        # Nouns do not have polarity but the Prague-style tagsets may mark it.
+        if node.upos in ['NOUN', 'PROPN']:
+            if node.feats['Polarity'] == 'Pos':
+                node.feats['Polarity'] = ''
+            elif node.feats['Polarity'] == 'Neg':
+                logging.warn(f'To remove Polarity=Neg from the NOUN {node.form}, we may have to change its lemma ({node.lemma}).')
+        # In 19th century data, the grammaticalized usages of "se", "si" are
+        # tagged as PART (rather than a reflexive PRON, which is the standard).
+        # Even if it already was tagged PRON, some features may have to be added.
+        if node.upos in ['PRON', 'PART'] and node.form.lower() in ['se', 'si']:
+            node.lemma = 'se'
+            node.upos = 'PRON'
+            node.feats['PronType'] = 'Prs'
+            node.feats['Reflex'] = 'Yes'
+            if node.form.lower() == 'se':
+                # Occasionally "se" can be genitive: "z prudkého do se dorážení".
+                if not node.feats['Case'] == 'Gen':
+                    node.feats['Case'] = 'Acc'
+            else:
+                node.feats['Case'] = 'Dat'
+            node.feats['Variant'] = 'Short'
         # In Czech UD, "být" is always tagged as AUX and never as VERB, regardless
         # of the fact that it can participate in purely existential constructions
         # where it no longer acts as a copula. Czech tagsets typically do not
