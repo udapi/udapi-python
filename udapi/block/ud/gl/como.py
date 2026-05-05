f615fdce84a40e16e9089afa4afb7d4136e27167
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-05-05 12:28:15 +0200

    Heuristics for UD_Galician-CTG.

---
+++ udapi/block/ud/gl/como.py
@@ -0,0 +1,24 @@
+"""
+Block ud.gl.Prep tries to solve some errors in Galician CTG where
+the conjunction "como" is mistakenly attached as a sibling of its phrase.
+
+Author: Dan Zeman
+"""
+from udapi.core.block import Block
+
+class Como(Block):
+    """Block for fixing "como" in UD_Galician-CTG."""
+
+    def process_node(self, node):
+        if node.form == 'como':
+            rightsiblings = node.siblings(following_only=True)
+            if len(rightsiblings) > 0:
+                if node.parent.ord < node.ord or node.parent.ord > rightsiblings[0].ord:
+                    node.parent = rightsiblings[0]
+                    # The conjunction is often mistagged as pronoun but we will not attempt to fix that now.
+                    node.deprel = 'mark' if node.upos == 'SCONJ' else 'nmod'
+            # Regardless whether "como" was already attached to the following
+            # phrase or we had to re-attach it, make sure that the phrase is
+            # not treated as a core argument.
+            if node.parent.ord > node.ord and node.parent.udeprel in ['nsubj', 'obj', 'iobj', 'csubj', 'ccomp']:
+                node.parent.deprel = 'advcl'
