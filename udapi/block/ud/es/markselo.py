749e713b90157842fc22059c1434120ee3012a48
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-06-26 16:42:16 +0200

    Requiring that se and lo are adjacent.

--- udapi/block/ud/es/markselo.py
+++ udapi/block/ud/es/markselo.py
@@ -13,10 +13,13 @@ class MarkSeLo(Block):
             if len(se_children) >= 1:
                 lo_children = [x for x in node.children if x.form.lower() in ['lo', 'la', 'los', 'las']]
                 if len(lo_children) >= 1:
-                    # We expect 1 se and 1 lo, not more.
-                    # Nevertheless, for the moment, mark all.
-                    for x in se_children:
-                        x.misc['Mark'] = 'se'
-                    for x in lo_children:
-                        x.misc['Mark'] = 'lo'
-                    node.misc['Mark'] = 'VERB'
+                    # Annotation errors lead to patterns like "los que se esperan".
+                    # Therefore, require that "se" and "lo" are adjacent.
+                    if abs(se_children[0].ord-lo_children[0].ord) == 1:
+                        # We expect 1 se and 1 lo, not more.
+                        # Nevertheless, for the moment, mark them all.
+                        for x in se_children:
+                            x.misc['Mark'] = 'se'
+                        for x in lo_children:
+                            x.misc['Mark'] = 'lo'
+                        node.misc['Mark'] = 'VERB'
